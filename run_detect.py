332d5a10b086dbe0f9d00937239e420698e6180e
Askura <asrikurniaramadhani48@gmail.com>
2026-05-22 09:33:07 +0700

    delete: graphify

--- run_detect.py
+++
@@ -1,8 +0,0 @@
-import json
-from graphify.detect import detect, _SKIP_DIRS
-from pathlib import Path
-
-_SKIP_DIRS.update({'node_modules', 'vendor', '.git'})
-
-result = detect(Path('.'))
-print(json.dumps(result))
